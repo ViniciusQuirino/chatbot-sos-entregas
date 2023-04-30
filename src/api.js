44557adebde0d31c8494350379cb00cd032ab249
ViniciusQuirino <viniciusquirino13@hotmail.com>
2023-04-30 14:59:36 -0300

    chatbot in progress

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://db-chatbot-sos.up.railway.app",
   timeout: 15000,
 });
 
