a60d05776fe0d8a3db8f8b55e13164c9e163437a
ViniciusQuirino <viniciusquirino13@hotmail.com>
2023-05-19 15:46:09 -0300

    Project Finished

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
 
 const api = axios.create({
-  // baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:3000",
   // baseURL: "https://database-sos.up.railway.app",
-  baseURL: "https://database-sos.cyclic.app",
+  // baseURL: "https://database-sos.cyclic.app",
   timeout: 15000,
 });
 
